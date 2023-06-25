c99ab25cfb3bcdb75bbbf4113ff41677b2d59cd4
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-06-25 12:19:18 -0500

    Add changes to the build in methods file

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,31 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include method is checking to see if the string contains the value input within the 
+# brackets. In this case the include is checking to see if "Hello" is present within the 
+# string "Helllo World". 
+# The return value will be a boolean and in this case it will return "true"
 "Hello World".include?("Hello")
 
-
+# The end_with? method is checking to see if the string ends with the value input within the
+# brackets. In this case the end with method will check to see if "Hello" is at the end of
+# the string.
+# The return value will be a boolean and in this case it will return "false"
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is checking to see if the string ends with the value input within the
+# brackets. In this case the end with method will check to see if "rld" is at the end of
+# the string.
+# The return value will be a boolean and in this case it will return "true"
 "Hello World".end_with?("rld")
 
-
+# The even? method below is checking to see if the integer before it is an even number.
+# The return value will be a boolean and in this case it will return "true"
 12.even?
 
-
+# The next method below will check the value of the integer before it.
+# The return will take the value of the original integer and state the next value ahead. In
+# this case it will return the value of 19
 18.next
 
 
@@ -30,6 +42,8 @@
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+NEED TO FINISH THIS SECTION!!!
 
 # EXAMPLE
 # The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
@@ -47,6 +61,15 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The next method below will check the value of the integer before it.
+# The return will take the value of the original integer and state the next value ahead. In
+# this case it will return the value of 29
+age = 28
+puts age.next
+# The even? method below is checking to see if the integer before it is an even number.
+# The return value will be a boolean and in this case it will return "true"
+favorite_num = 4
+puts favorite_num.even?
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +77,14 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The include? method is checking to see if the string contains with the value input within 
+# the brackets. In this case the method will check to see if "Justice" is within the array.
+# The return value will be a boolean and in this case it will return "true"
+virtues = ["Wisdom", "Courage", "Temperance", "Justice"]
+puts virtues.include?("Justice")
+# The first method is checking to see what value is first in the array. In this case the 
+# method will check the array to find the value "Socrates". 
+# The return value will be a string and in this case it will return "Socrates"
+philosophers = ["Socrates", "Plato", "Aristotle"]
+puts philosophers.first
